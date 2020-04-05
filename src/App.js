1619be440f49f29a159159ac4796a853bb5a51f4
Bryson Creighton <brysoncreighton@gmail.com>
2020-04-05 15:57:30 -0600

    Update component foundational folder format

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-
+        <header className="header">
+          <h1 className='AppTitle'>Noteful</h1>
         </header>
       </div>
     )
