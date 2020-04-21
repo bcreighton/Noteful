1b2ea73f8e432ba061ab1e94e0ed5c3c2442b697
Bryson Creighton <brysoncreighton@gmail.com>
2020-04-21 13:29:16 -0600

    Update main.js with commented context and pass context consumer code

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react'
+import NotefulContext from '../../NotefulContext'
 import { withRouter } from 'react-router-dom'
 import NoteList from '../noteList/NoteList'
 import './Main.css'
 
 class Main extends Component {
+  static contextType = NotefulContext;
+
   getNotesById() {
+    // Refactored code using context
+
+    /* 
+    return this.props.notes.filter(note => note.folderId === this.props.match.params.folderId);
+    */
     return this.props.notes.filter(note => note.folderId === this.props.match.params.folderId);
   }
 
   render() {
     return (
+      // Refactored code using context
+      /* <NotefulContext.Consumer>
+        {(context) => {
+          <section className='mainSection'>
+            {
+              this.context.match.params.folderId
+                ? <NoteList
+                  notes={this.getNotesById()}
+                />
+                : <NoteList
+                  notes={this.context.notes}
+                />
+            }
+          </section>
+        }}
+      </NotefulContext.Consumer> */
       <section className='mainSection'>
         {
           this.props.match.params.folderId
