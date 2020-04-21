6a5ded2051d1b202be4f67db7e6ff536d5450bbd
Bryson Creighton <brysoncreighton@gmail.com>
2020-04-21 00:01:18 -0600

    Update Delete.js with commented code using context and calling delete API request

--- src/components/buttons/delete/Delete.js
+++ src/components/buttons/delete/Delete.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
+import NotefulContext from '../../../NotefulContext'
 import '../Button.css'
 import './Delete.css'
 
-export default class Delete extends Component {
+class Delete extends Component {
+  static contextType = NotefulContext;
+
   render() {
     return (
+      // New refactored code using context
+      /* 
+      <NotefulContext.Consumer>
+        {(context) => (
+          <button 
+            id='delete'
+            className='btn deleteBTN'
+            onClick={() => {
+              deleteNoteRequest(
+                props.id,
+                context.deleteNote,
+              )
+            }}
+          >
+          Delete
+          </button> 
+        )}
+      </NotefulContext.Consumer>
+      */
       <button id='delete' className='btn deleteBTN'>Delete</button>
     )
   }
 }
+
+export default Delete
