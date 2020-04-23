345a70d30af6a56b594800f7ef9adfdfd8730fbf
Bryson Creighton <brysoncreighton@gmail.com>
2020-04-22 23:46:53 -0600

    Update GoBack.js house the goBack function directly

--- src/components/buttons/goBack/GoBack.js
+++ src/components/buttons/goBack/GoBack.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
+import NotefulContext from '../../../NotefulContext'
 import '../Button.css'
 import './GoBack.css'
 
 export default class GoBack extends Component {
+  static contextType = NotefulContext;
+
+  /* 
+  goBackClick = () => {
+    this.props.history.goBack()
+  }
+  */
+
   render() {
     return (
       <button
         id='goBack'
         className='btn'
+        /* onClick={this.goBackClick} */
         onClick={this.props.goBackClick}
       >Go Back</button>
     )
