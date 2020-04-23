019734864f16b5fa19f9249cd5f38b6aef2e01b0
Bryson Creighton <brysoncreighton@gmail.com>
2020-04-22 23:54:47 -0600

    Update SideBarActiveNote.js with commented context code

--- src/components/sideBarActiveNote/SideBarActiveNote.js
+++ src/components/sideBarActiveNote/SideBarActiveNote.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import NotefulContext from '../../NotefulContext'
 import { withRouter } from 'react-router-dom'
 import GoBack from '../buttons/goBack/GoBack'
 import './SideBarActiveNote.css'
 
 class SideBarActiveNote extends Component {
+  static contextType = NotefulContext;
+
   getFolder() {
+    /* 
+    const selectedNote = this.context.notes.find(note => note.id === this.context.match.params.noteId)
+    
+    return this.context.folders.find(folder => folder.id === selectedNote.folderId)
+    */
     const selectedNote = this.props.notes.find(note => note.id === this.props.match.params.noteId)
+
     return this.props.folders.find(folder => folder.id === selectedNote.folderId)
   }
 
@@ -13,6 +22,7 @@ class SideBarActiveNote extends Component {
     const selectedFolder = this.getFolder()
     return (
       <aside className='sideBar activeNote'>
+        {/* <GoBack /> */}
         <GoBack
           goBackClick={this.props.goBackClick}
         />
