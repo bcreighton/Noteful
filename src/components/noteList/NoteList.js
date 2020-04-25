d237bbaf530640939d20d63be2045d45c2b29448
Bryson Creighton <brysoncreighton@gmail.com>
2020-04-25 15:28:56 -0600

    Add context regactor code to NoteList.js

--- src/components/noteList/NoteList.js
+++ src/components/noteList/NoteList.js
@@ -7,12 +7,10 @@ import './NoteList.css'
 export default class NoteList extends Component {
   static contextType = NotefulContext;
 
-  // Refactored code using context
-  /* 
   getNotesById() {
     return this.context.notes.filter(note => note.folderId === this.context.match.params.folderId);
-  } 
-  
+  }
+
   generateNotesList(notes) {
     return notes.map(note => (
       <NoteListItem
@@ -25,29 +23,15 @@ export default class NoteList extends Component {
       />
     ))
   }
-  */
 
   render() {
-    /* 
     const noteListItems = this.context.match.params.folderId
       ? this.getNotesById()
       : this.context.notes
-    */
-    const noteListItems = this.props.notes.map(note => (
-      <NoteListItem
-        key={note.id}
-        id={note.id}
-        folderId={note.folderId}
-        title={note.name}
-        modDate={note.modified}
-        content={note.content}
-      />
-    ))
 
     return (
       <>
-        {/* {this.generateNotesList(noteListItems)} */}
-        {noteListItems}
+        {this.generateNotesList(noteListItems)}
         <AddNote />
       </>
     )
